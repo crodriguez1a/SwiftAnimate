475a5dd95cfd01173b2114ba247f024a1300f0ca
crodriguez1a <crodriguez1a@gmail.com>
2015-02-09 19:53:16 -0500

    Moving to optional paramaters
    - optional params
    - omission key 0.001 and 0 because 0 is a commonly used value

--- Animate.swift
+++ Animate.swift
@@ -12,71 +12,89 @@ import UIKit
 
 class Animate {
   var animator: UIDynamicAnimator!
-  func move(delay: NSTimeInterval, reference: UIView, view: UIView, x: CGFloat, y: CGFloat, alpha: CGFloat, w:CGFloat, h:CGFloat) {
+  func move(delay: NSTimeInterval, reference: UIView, view: UIView, x: CGFloat = 0.001, y: CGFloat = 0.001, alpha: CGFloat = 0.001, w:CGFloat = 0.001, h:CGFloat = 0.001, callback: (finished:Bool) -> ()) {
+    
 
     animator = UIDynamicAnimator(referenceView: reference)
     UIView.animateWithDuration(delay, animations: {
-      if(x != 0) {
+      
+      if(x != 0.001) {
         view.frame.origin.x = x
       }
-      if(y != 0) {
+      if(y != 0.001) {
         view.frame.origin.y = y
       }
-
-      if(w != 0) {
+      if(w != 0.001) {
         view.bounds.size.width = w
       }
-
-      if(h != 0) {
+      if(h != 0.001) {
         view.bounds.size.height = h
       }
-
       view.alpha = alpha
-
+      
     }, completion: {
     (finished: Bool) in
-      //println("animation complete")
-      /* could add callback references to this as well */
+      callback(finished: true)
     })
-
+  
+    var snapBehavior: UISnapBehavior = UISnapBehavior(item: view, snapToPoint: reference.center)
+    animator.addBehavior(snapBehavior)
+  }
+  
+  func newImage(delay: NSTimeInterval, reference: UIView, view: UIImageView, img: String) {
+    
+    animator = UIDynamicAnimator(referenceView: reference)
+    UIView.animateWithDuration(delay, animations: {
+        view.image = UIImage(named: img)
+      }, completion: {
+        (finished: Bool) in
+        //fin
+    })
+    
     var snapBehavior: UISnapBehavior = UISnapBehavior(item: view, snapToPoint: reference.center)
     animator.addBehavior(snapBehavior)
   }
 }
 
+extension UIImageView {
+  public func swap(img:String, delay:NSTimeInterval, reference: UIView) {
+    Animate().newImage(delay, reference: reference, view: self, img: img)
+  }
+}
+
 extension UIView {
-  public func grow(w:CGFloat, h:CGFloat, delay:NSTimeInterval, reference: UIView) {
-    Animate().move(delay, reference:reference, view:self, x:0, y:0, alpha: self.alpha, w:w, h:h)
+  public func grow(w:CGFloat, h:CGFloat, delay:NSTimeInterval, reference: UIView, callback: (finished: Bool) -> ()) {
+    Animate().move(delay, reference:reference, view:self, alpha: self.alpha, w:w, h:h, callback:callback)
   }
-  public func moveY(y:CGFloat, delay:NSTimeInterval, reference: UIView) {
-    Animate().move(delay, reference:reference, view:self, x:0, y:y, alpha: self.alpha, w:0, h:0)
+  public func moveY(y:CGFloat, delay:NSTimeInterval, reference: UIView, callback: (finished: Bool) -> ()) {
+    Animate().move(delay, reference:reference, view:self, y:y, alpha: self.alpha, callback:callback)
   }
-  public func moveX(x:CGFloat, delay:NSTimeInterval, reference: UIView) {
-    Animate().move(delay, reference:reference, view:self, x:x, y:0, alpha: self.alpha, w:0, h:0)
+  public func moveX(x:CGFloat, delay:NSTimeInterval, reference: UIView, callback: (finished: Bool) -> ()) {
+    Animate().move(delay, reference:reference, view:self, x:x, alpha: self.alpha, callback:callback)
   }
-  public func moveRight(x:CGFloat, delay:NSTimeInterval, reference: UIView) {
-    Animate().move(delay, reference:reference, view:self, x:self.frame.origin.x + x, y:0, alpha: self.alpha, w:0, h:0)
+  public func moveRight(x:CGFloat, delay:NSTimeInterval, reference: UIView, callback: (finished: Bool) -> ()) {
+    Animate().move(delay, reference:reference, view:self, x:self.frame.origin.x + x, alpha: self.alpha, callback:callback)
   }
-  public func moveLeft(x:CGFloat, delay:NSTimeInterval, reference: UIView) {
-    Animate().move(delay, reference:reference, view:self, x:self.frame.origin.x - x, y:0, alpha: self.alpha, w:0, h:0)
+  public func moveLeft(x:CGFloat, delay:NSTimeInterval, reference: UIView, callback: (finished: Bool) -> ()) {
+    Animate().move(delay, reference:reference, view:self, x:self.frame.origin.x - x, alpha: self.alpha, callback:callback)
   }
-  public func moveUp(y:CGFloat, delay:NSTimeInterval, reference: UIView) {
-    Animate().move(delay, reference:reference, view:self, x:0, y:self.frame.origin.y - y, alpha: self.alpha, w:0, h:0)
+  public func moveUp(y:CGFloat, delay:NSTimeInterval, reference: UIView, callback: (finished: Bool) -> ()) {
+    Animate().move(delay, reference:reference, view:self, y:self.frame.origin.y - y, alpha: self.alpha, callback:callback)
   }
-  public func moveDown(y:CGFloat, delay:NSTimeInterval, reference: UIView) {
-    Animate().move(delay, reference:reference, view:self, x:0, y:self.frame.origin.y + y, alpha: self.alpha, w:0, h:0)
+  public func moveDown(y:CGFloat, delay:NSTimeInterval, reference: UIView, callback: (finished: Bool) -> ()) {
+    Animate().move(delay, reference:reference, view:self, y:self.frame.origin.y + y, alpha: self.alpha, callback:callback)
   }
-  public func fade(alpha:CGFloat, delay:NSTimeInterval, reference: UIView) {
-    Animate().move(delay, reference:reference, view:self, x:0, y:0, alpha: alpha, w:0, h:0)
+  public func fade(alpha:CGFloat, delay:NSTimeInterval, reference: UIView, callback: (finished: Bool) -> ()) {
+    Animate().move(delay, reference:reference, view:self, alpha: alpha, callback:callback)
   }
-  public func fadeOut(delay:NSTimeInterval, reference: UIView) {
-    Animate().move(delay, reference:reference, view:self, x:0, y:0, alpha: 0, w:0, h:0)
+  public func fadeOut(delay:NSTimeInterval, reference: UIView, callback: (finished: Bool) -> ()) {
+    Animate().move(delay, reference:reference, view:self, callback:callback)
   }
-  public func fadeIn(delay:NSTimeInterval, reference: UIView) {
-    Animate().move(delay, reference:reference, view:self, x:0, y:0, alpha: 1, w:0, h:0)
+  public func fadeIn(delay:NSTimeInterval, reference: UIView, callback: (finished: Bool) -> ()) {
+    Animate().move(delay, reference:reference, view:self, callback:callback)
   }
-
-
+  
+  
 }
 
 
@@ -84,5 +102,5 @@ extension UIView {
 //Usage
 var delay:NSTimeInterval = NSTimeInterval(0.25)
 _myView.fadeIn(delay, reference: view)
-_myView.moveY(settingsorigin, delay: delay, reference: view)
+_myView.moveY(100, delay: NSTimeInterval(0.25), reference: view)
 */
